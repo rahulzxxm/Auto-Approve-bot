fcb29cec2f8f0cc718dc604b2941d218351381d5
rahulzxxm <rahulchouhan07815@gmail.com>
2025-08-10 20:06:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "22609670"))
+    API_HASH = getenv("API_HASH", "3506d8474ad1f4f5e79b7c52a5c3e88d")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7680901418:AAGoc-69uZG_V7TAgre5ssBKK17ZRgRMfo4")
+    FSUB = getenv("FSUB", "Tigerxy09")
+    CHID = int(getenv("CHID", "-1002215906991"))
+    SUDO = list(map(int, getenv("SUDO", "5881684718 123456789").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://tigerzx:tiger99@cluster0.f1nph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
